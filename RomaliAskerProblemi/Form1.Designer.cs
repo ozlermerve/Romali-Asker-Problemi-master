e907da521474e34c094d3990000089772fe21f6e
harun uysal <harun_fatih@hotmail.com>
2017-12-11 12:08:57 +0300

    Initial commit

---
+++ RomaliAskerProblemi/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RomaliAskerProblemi
+{
+    partial class frmRomaliAsker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKalanAsker = new System.Windows.Forms.Button();
+            this.lblAskerSayisi = new System.Windows.Forms.Label();
+            this.lblAdimSayisi = new System.Windows.Forms.Label();
+            this.txtAskerSayisi = new System.Windows.Forms.TextBox();
+            this.txtAdimSayisi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKalanAsker
+            // 
+            this.btnKalanAsker.Location = new System.Drawing.Point(88, 95);
+            this.btnKalanAsker.Name = "btnKalanAsker";
+            this.btnKalanAsker.Size = new System.Drawing.Size(138, 23);
+            this.btnKalanAsker.TabIndex = 0;
+            this.btnKalanAsker.Text = "Kalan Askerleri Bul";
+            this.btnKalanAsker.UseVisualStyleBackColor = true;
+            this.btnKalanAsker.Click += new System.EventHandler(this.btnKalanAsker_Click);
+            // 
+            // lblAskerSayisi
+            // 
+            this.lblAskerSayisi.AutoSize = true;
+            this.lblAskerSayisi.Location = new System.Drawing.Point(12, 26);
+            this.lblAskerSayisi.Name = "lblAskerSayisi";
+            this.lblAskerSayisi.Size = new System.Drawing.Size(73, 13);
+            this.lblAskerSayisi.TabIndex = 1;
+            this.lblAskerSayisi.Text = "Asker Sayısı : ";
+            // 
+            // lblAdimSayisi
+            // 
+            this.lblAdimSayisi.AutoSize = true;
+            this.lblAdimSayisi.Location = new System.Drawing.Point(13, 61);
+            this.lblAdimSayisi.Name = "lblAdimSayisi";
+            this.lblAdimSayisi.Size = new System.Drawing.Size(69, 13);
+            this.lblAdimSayisi.TabIndex = 2;
+            this.lblAdimSayisi.Text = "Adım Sayısı : ";
+            // 
+            // txtAskerSayisi
+            // 
+            this.txtAskerSayisi.Location = new System.Drawing.Point(88, 23);
+            this.txtAskerSayisi.Name = "txtAskerSayisi";
+            this.txtAskerSayisi.Size = new System.Drawing.Size(138, 20);
+            this.txtAskerSayisi.TabIndex = 3;
+            // 
+            // txtAdimSayisi
+            // 
+            this.txtAdimSayisi.Location = new System.Drawing.Point(88, 58);
+            this.txtAdimSayisi.Name = "txtAdimSayisi";
+            this.txtAdimSayisi.Size = new System.Drawing.Size(138, 20);
+            this.txtAdimSayisi.TabIndex = 4;
+            // 
+            // frmRomaliAsker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 135);
+            this.Controls.Add(this.txtAdimSayisi);
+            this.Controls.Add(this.txtAskerSayisi);
+            this.Controls.Add(this.lblAdimSayisi);
+            this.Controls.Add(this.lblAskerSayisi);
+            this.Controls.Add(this.btnKalanAsker);
+            this.Name = "frmRomaliAsker";
+            this.Text = "Romalı Asker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKalanAsker;
+        private System.Windows.Forms.Label lblAskerSayisi;
+        private System.Windows.Forms.Label lblAdimSayisi;
+        private System.Windows.Forms.TextBox txtAskerSayisi;
+        private System.Windows.Forms.TextBox txtAdimSayisi;
+    }
+}
+
---
+++ RomaliAskerProblemi/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomaliAskerProblemi
+{
+    public partial class frmRomaliAsker : Form
+    {
+        public frmRomaliAsker()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKalanAsker_Click(object sender, EventArgs e)
+        {
+            if (txtAskerSayisi.Text == "" || txtAdimSayisi.Text == "")
+                MessageBox.Show("Gerekli alanları doldurun.");
+            else
+            {
+                string textAskerSayisi = txtAskerSayisi.Text, textAdimSayisi = txtAdimSayisi.Text;
+                int kontrolas = 1, kontrolad = 1;
+
+                foreach (char askerIndis in textAskerSayisi)
+                {
+                    if (askerIndis >= 48 && askerIndis <= 57)
+                        continue;
+                    else
+                        kontrolas = 0;
+                }
+
+                foreach (char adimIndis in textAdimSayisi)
+                {
+                    if (adimIndis >= 48 && adimIndis <= 57)
+                        continue;
+                    else
+                        kontrolad = 0;
+                }
+
+                if (kontrolas == 0)
+                    MessageBox.Show("Asker sayısını numeric bir değer girin!");
+                else if (kontrolad == 0)
+                    MessageBox.Show("Adım sayısını numeric bir değer girin!");
+                else
+                {
+                    int askerSayisi = Convert.ToInt32(txtAskerSayisi.Text);
+                    int adimSayisi = Convert.ToInt32(txtAdimSayisi.Text);
+
+                    if (askerSayisi <= 2)
+                        MessageBox.Show("Asker sayısı 2 ve 2'den küçük olamaz.");
+                    else if (adimSayisi == 0)
+                        MessageBox.Show("Adım sayısı 0 olamaz.");
+                    else
+                    {
+                        int askerKont = askerSayisi;
+                        int[] askerler = new int[askerSayisi];
+
+                        for (int j = 0; j < askerSayisi; j++)
+                        {
+                            askerler[j] = 1; //Tüm askerler başta yaşıyorlar yani değerleri 1
+                        }
+
+                        int askerKontrol = 0, adimKont = 1;
+                        while (askerKont != 2) //Son 2 asker kalmadığı sürece dön
+                        {
+                            if (askerKontrol == askerSayisi) //son indisli askere geldiysen 0. indise geri dön
+                                askerKontrol = 0;
+                            if (askerler[askerKontrol] == 1) //Asker yaşıyorsa
+                            {
+                                if (adimKont % adimSayisi == 0) //askerler üzerinde adım sayısı kadar ilerlendiyse yani ölecek asker bulundu.
+                                {
+                                    askerler[askerKontrol] = 0;
+                                    askerKont--;
+                                    adimKont = 1; //adım sayısı 1'e geri dönsün ki diğer ölecek asker için tekrar adım hesaplansın
+                                }
+                                else
+                                    adimKont++; //Asker ölmedi daha gidilecek adım var ilerle
+                                askerKontrol++;
+                            }
+                            else
+                                askerKontrol++; //bir sonra ki askere bakmak için ilerle
+                        }
+                        string mesaj = "";
+                        //kalan askerleri göster
+                        for (int k = 0; k < askerSayisi; k++)
+                        {
+                            if (askerler[k] == 1)
+                                mesaj += (k + 1).ToString() + Environment.NewLine;
+                        }
+                        MessageBox.Show(mesaj);
+                    }
+                }
+            }
+        }
+    }
+}
